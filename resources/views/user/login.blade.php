a46210717968616d6f4088c45919fd8b183b2b96
RofiNr <rofinr2002@students.unnes.ac.id>
2023-05-23 10:45:58 +0700

    up

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -24,9 +24,13 @@
                                     <div class="text-center">
                                         <h4 class="text-white mb-4">Welcome Back!</h4>
                                     </div>
+                                    <form action="{{ route('login.action') }}" method="POST">
+                                        @csrf
                                     <form class="user">
-                                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email"></div>
-                                        <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password"></div>
+                                        <div class="mb-3"><input class="form-control" type="username" name="username" value="{{ old('username') }}" />
+                                        </div>
+                                        <div class="mb-3"><input class="form-control" type="password" name="password" />
+                                        </div>
                                         <div class="mb-3">
                                             <div class="custom-control custom-checkbox small">
                                                 <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
@@ -36,7 +40,7 @@
                                         <hr>
                                     </form>
                                     <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
-                                    <div class="text-center"><a class="small" href="register.html">Create an Account!</a></div>
+                                    <div class="text-center"><a class="small" href="{{ route('register') }}">Create an Account!</a></div>
                                 </div>
                             </div>
                         </div>
